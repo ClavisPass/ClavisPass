5a71f0548121da88cef96272bc34142b7ae4cc71
RiciRic <ricivrdrm99@hotmail.de>
2025-05-29 22:33:54 +0200

    updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -52,6 +52,7 @@ fn load_window_size(app_handle: &AppHandle) -> Option<WindowSize> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
--- src/components/UpdateManager.tsx
+++ src/components/UpdateManager.tsx
@@ -1,19 +1,28 @@
-import { useEffect, useState } from 'react';
-import { Platform, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { Platform, View } from "react-native";
 
 import { Text, Button } from "react-native-paper";
 
 // Expo Updates für Mobile
-import * as Updates from 'expo-updates';
+import * as Updates from "expo-updates";
 // Tauri Updater für Desktop
-//import { checkUpdate, installUpdate } from '@tauri-apps/plugin-updater'; //TODO: Ensure this is installed in your Tauri project
+import { check, Update as UpdateProp } from "@tauri-apps/plugin-updater";
+import { relaunch } from "@tauri-apps/plugin-process";
 
 const UpdateManager = () => {
   const [updateAvailable, setUpdateAvailable] = useState(false);
-  const [updateMessage, setUpdateMessage] = useState('Searching for updates...');
+  const [updateMessage, setUpdateMessage] = useState(
+    "Searching for updates..."
+  );
+
+  const [getContentLength, setContentlength] = useState<number | undefined>(
+    undefined
+  );
+  const [downloaded, setDownloaded] = useState<number | undefined>(undefined);
+  const [update, setUpdate] = useState<UpdateProp | null>(null);
 
   useEffect(() => {
-    if (Platform.OS === 'web') {
+    if (Platform.OS === "web") {
       checkTauriUpdate();
     } else {
       checkExpoUpdate();
@@ -25,10 +34,10 @@ const UpdateManager = () => {
       const update = await Updates.checkForUpdateAsync();
       if (update.isAvailable) {
         setUpdateAvailable(true);
-        setUpdateMessage('Update Available');
+        setUpdateMessage("Update Available");
       }
     } catch (error) {
-      setUpdateMessage('Error while checking for updates');
+      setUpdateMessage("Error while checking for updates");
     }
   };
 
@@ -37,46 +46,82 @@ const UpdateManager = () => {
       await Updates.fetchUpdateAsync();
       await Updates.reloadAsync();
     } catch (error) {
-      setUpdateMessage('Error while applying update');
+      setUpdateMessage("Error while applying update");
     }
   };
 
   const checkTauriUpdate = async () => {
     try {
-      //const { shouldUpdate } = await checkUpdate();
-      /*if (shouldUpdate) {
+      setUpdate(await check());
+      if (update) {
         setUpdateAvailable(true);
-        setUpdateMessage('Update Available');
-      }*/
+        setUpdateMessage("Update Available");
+      }
     } catch (error) {
-      setUpdateMessage('Error while checking for updates');
+      setUpdateMessage("Error while checking for updates");
       console.log("error", error);
     }
   };
 
   const applyTauriUpdate = async () => {
     try {
-      //await installUpdate();
+      if (update) {
+        console.log(
+          `found update ${update.version} from ${update.date} with notes ${update.body}`
+        );
+        let downloaded = 0;
+        await update.downloadAndInstall((event) => {
+          switch (event.event) {
+            case "Started":
+              setContentlength(event.data.contentLength);
+              console.log(
+                `started downloading ${event.data.contentLength} bytes`
+              );
+              break;
+            case "Progress":
+              downloaded += event.data.chunkLength;
+              setDownloaded(downloaded);
+              console.log(`downloaded ${downloaded} from ${getContentLength}`);
+              break;
+            case "Finished":
+              console.log("download finished");
+              break;
+          }
+        });
+
+        console.log("update installed");
+        await relaunch();
+      }
     } catch (error) {
       console.error(error);
-      setUpdateMessage('Error while applying update');
+      setUpdateMessage("Error while applying update");
     }
   };
 
   const applyUpdate = async () => {
-    if (Platform.OS === 'web') {
+    if (Platform.OS === "web") {
       applyTauriUpdate();
     } else {
       applyExpoUpdate();
     }
   };
 
+  useEffect(() => {
+    if (update) {
+      setUpdateAvailable(true);
+      setUpdateMessage("Update Available");
+    }
+  }, [update]);
 
   return (
     <View style={{ padding: 20, width: "50%" }}>
       <Text>{updateMessage}</Text>
+      <Text>{getContentLength}</Text>
+      <Text>{downloaded}</Text>
       {updateAvailable && (
-        <Button mode='contained' onPress={applyUpdate}>Update</Button>
+        <Button mode="contained" onPress={applyUpdate}>
+          Update
+        </Button>
       )}
     </View>
   );
