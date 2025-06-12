3e3a2a92cbea6f876165c83398dc29d45d6c64ed
RiciRic <ricivrdrm99@hotmail.de>
2025-06-12 23:26:28 +0200

    prebuild clear

--- android/app/src/main/java/com/ClavisPass/ClavisPass/MainActivity.kt
+++ android/app/src/main/java/com/ClavisPass/ClavisPass/MainActivity.kt
@@ -1,5 +1,5 @@
-import expo.modules.splashscreen.SplashScreenManager
 package com.ClavisPass.ClavisPass
+import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
 import android.os.Bundle
