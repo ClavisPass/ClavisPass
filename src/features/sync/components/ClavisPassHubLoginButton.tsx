5acca5ca92a444ba3ed7c2f317cf5ee393595637
RiciRic <ricivrdrm99@hotmail.de>
2026-03-27 17:03:18 +0100

    button styling

--- src/features/sync/components/ClavisPassHubLoginButton.tsx
+++ src/features/sync/components/ClavisPassHubLoginButton.tsx
@@ -2,7 +2,6 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import { View } from "react-native";
 import {
   ActivityIndicator,
-  Button as PaperButton,
   Icon,
   Portal,
   Text,
@@ -348,10 +347,6 @@ function ClavisPassHubLoginButton() {
               loading={loading}
               disabled={loading}
             />
-
-            <PaperButton disabled={loading} onPress={() => setVisible(false)}>
-              {t("common:cancel")}
-            </PaperButton>
           </View>
         </Modal>
       </Portal>
